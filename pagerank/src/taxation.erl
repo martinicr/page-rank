3bb61130d8712295c3ddbdc7d314dc823b670d2e
Martin Flores <martin.flores@bodybuilding.com>
2015-11-05 23:59:28 -0600

    Agregando configuracion de multi-nodos

--- pagerank/src/taxation.erl
+++ pagerank/src/taxation.erl
@@ -167,8 +167,8 @@ tax() ->
   tax([0.25,0.25,0.25,0.25], ?BETA, ?ADJ_LIST, ?N, ?K, Red_Proc, Mappers).
 
 tax(Vector) ->
-  Red_Proc = file_based_exec('tres@mf-bbcom'),
-  Mappers = file_based_processes(['dos@mf-bbcom']),
+  Red_Proc = file_based_exec('reducer@reducer-pr'),
+  Mappers = file_based_processes(['uno@node1-pr','dos@node2-pr']),
   tax(Vector, ?BETA, ?ADJ_LIST, ?N, ?K, Red_Proc, Mappers).
 
 tax(Vector, Beta, Adj_List, N, K, Red_Proc, Map_Proc) ->
